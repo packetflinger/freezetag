ad3127be61bc5fdc55c8fe612b35aaabf3243776
Joe Reid <joe@joereid.com>
2021-12-27 21:56:20 -0500

    initial import

---
+++ banner.h
@@ -0,0 +1,18 @@
+// Banner text
+
+// Added by QwazyWabbit to keep it all in one place
+
+#ifndef BANNER
+
+static char banner[] = 
+{
+	"|=========================================================|\n"
+	"|                 Welcome to FreezeTag                    |\n"
+	"|       Version "VERSION" "BUILD" "__DATE__" "__TIME__"      |\n" //looks weird, prints right
+	"|=========================================================|\n"
+	"|      This version is maintained by QwazyWabbit<WOS>     |\n"
+	"|=========================================================|\n"
+};
+
+#define BANNER
+#endif
---
+++ freeze.h
@@ -0,0 +1,90 @@
+struct {
+	int	score;
+	int	thawed;
+	float	win_time;
+	qboolean	update;
+	float	last_update;
+	int	frozen;
+	int	alive;
+	float	break_time;
+	qboolean	ready;
+} freeze[5];
+
+typedef struct hnode {
+	struct pnode*	entries;
+	int	cur;
+	int	num;
+} hndnode;
+
+typedef struct pnode {
+	char*	text;
+	int	align;
+	void*	arg;
+	void	(*selectfunc)(edict_t* ent, struct pnode* entry);
+} pmenunode;
+
+struct {
+	char*	name;
+	char*	gravity;
+	int	light;
+	qboolean	did;
+	qboolean	hook;
+} maplist[64];
+
+enum team_enum {
+	red, blue, green, yellow, none };
+static char*	freeze_team[] = {"Red", "Blue", "Green", "Yellow", "None"};
+int	endMapIndex;
+
+void gibThink(edict_t* ent);
+void pmenu_next(edict_t* ent);
+void pmenu_prev(edict_t* ent);
+void pmenu_close(edict_t* ent);
+void pmenuBegin(edict_t* ent);
+void pmenu_select(edict_t* ent);
+void pmenu_update(edict_t* ent);
+void sayTeam(edict_t* who);
+void cmdHook(edict_t* ent);
+void cmdFlashlight(edict_t* ent);
+void cmdChange(edict_t* ent);
+void cmdPlay(edict_t* ent);
+void cmdVote(edict_t* ent);
+void cmdReady(edict_t* ent);
+qboolean playerDamage(edict_t* targ, edict_t* attacker, int damage);
+void freezeRespawn(edict_t* ent, float delay);
+qboolean freezeMap();
+qboolean endCheck();
+qboolean gibCheck();
+void playerShell(edict_t* ent, int team);
+void cvarFreeze();
+void freezeSpawn();
+qboolean freezeCheck(edict_t* ent);
+void freezeAnim(edict_t* ent);
+void playerWeapon(edict_t* ent);
+void freezeSkin(edict_t* ent, char* s);
+void freezeMain(edict_t* ent);
+void cmdMoan(edict_t* ent);
+void freezeIntermission(void);
+void freezeScore(edict_t* ent, edict_t* killer);
+void playerStat(edict_t* ent);
+void freezeEffects(edict_t* ent);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
